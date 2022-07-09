ed314e76ee2f24742960aedbd35ca8fdffafef85
Caroline Lienard <carolinemrd@outlook.fr>
2022-07-09 10:53:25 +0200

    ⚡️New features

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,28 @@ import Cart from './Cart'
 import ShoppingList from './ShoppingList'
 import Footer from './Footer'
 
-import logo from "../assets/leaf.png"
+import { useState, useEffect } from 'react'
 
+import logo from "../assets/leaf.png"
 import '../styles/Layout.css'
 
 function App() {
+	const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+    
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
 	return (
 		<div>
 			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
 			<div className='lmj-layout-inner'>
-				<Cart />
-				<ShoppingList />
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
 			</div>
 			<Footer />
 		</div>
